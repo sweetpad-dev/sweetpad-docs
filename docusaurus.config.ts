ace9cebd17f1e8f8f1fc5bdea9f4e038563f46ef
Yevhenii Hyzyla <hyzyla@gmail.com>
2024-09-12 23:08:16 +0200

    Add links to markertplace and edit page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sweetpad-dev/sweetpad-docs/edit/main/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -93,6 +93,11 @@ const config: Config = {
         {
           href: 'https://github.com/sweetpad-dev/sweetpad',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://marketplace.visualstudio.com/items?itemName=sweetpad.sweetpad',
+          label: 'VSCode Marketplace',
           position: 'right',
         },
       ],
