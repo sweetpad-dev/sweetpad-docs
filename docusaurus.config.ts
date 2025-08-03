4fab0dfefd0fa352c8158e3cfcc849a922f1d1d1
Yevhenii Hyzyla <hyzyla@gmail.com>
2025-08-03 17:02:07 +0200

    Add blog post entry

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        // {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/sweetpad-dev/sweetpad',
           label: 'GitHub',
@@ -117,10 +117,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
             {
               label: 'GitHub',
               href: 'https://github.com/sweetpad-dev/sweetpad',
